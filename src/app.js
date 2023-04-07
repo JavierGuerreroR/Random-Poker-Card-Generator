9c250b90f8f25ad0f963665ca71d7f15e1322c46
JavierGuerreroR <javierguerrerorosas@gmail.com>
2023-04-07 12:10:20 +0000

    card generator 1

--- src/app.js
+++ src/app.js
@@ -7,4 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suit = ["♦", "♥", "♠", "♣"];
+
+  let randomNumber = number[Math.floor(Math.random() * 12)];
+  let randomSuit = Math.floor(Math.random() * 3);
+
+  document.getElementById("card").innerHTML = `<div
+  class="card d-flex justify-content-between container-fluid"
+  style="width: 18rem; height: 25rem;"
+>
+  <div class= "display-1" style='${
+    randomSuit <= 1 ? "color: red;" : ""
+  }' id="stick">${suit[randomSuit]}</div>
+  <div class="display-1 text-center fw-bold" style='${
+    randomSuit <= 1 ? "color: red;" : ""
+  }' >${randomNumber}</div>
+  <div class="text-end display-1" style='${
+    randomSuit <= 1 ? "color: red;" : ""
+  } transform: rotate(180deg) scaleX(-1); '>${suit[randomSuit]}</div>
+  </div>`;
 };
